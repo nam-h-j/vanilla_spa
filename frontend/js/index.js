72c1f53eb9bf79e7fb48774d5e9321627e2c7f91
nam <hzzn74@gmail.com>
2022-03-02 23:23:29 +0900

    set index.js and bring it on server

---
+++ frontend/js/index.js
@@ -0,0 +1,1 @@
+console.log("im index.js");
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require("path");
 
 const app = express();
 
+//4. index.js를 사용하도록 설정
+app.use("/js", express.static(path.resolve("frontend", "js")));
+
+// 3. frontend 디렉토리의 index.html을 가져옴
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve("frontend", "index.html"));
 });
